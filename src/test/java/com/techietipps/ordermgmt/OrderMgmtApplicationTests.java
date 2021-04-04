978f977c22375f8011dcd2a724d9339f946c82e6
Naveen Kumar Anem <naveenanem22@gmail.com>
2021-04-04 17:17:05 +0530

    Integration Tests with Test Profile configured
    Integration Tests with Test Profile configured

--- src/test/java/com/techietipps/ordermgmt/OrderMgmtApplicationTests.java
+++ src/test/java/com/techietipps/ordermgmt/OrderMgmtApplicationTests.java
@@ -1,13 +1,25 @@
 package com.techietipps.ordermgmt;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@Profile("test")
+@ActiveProfiles("test")
 class OrderMgmtApplicationTests {
+
+	@Value("${sample}")
+	private String sampleProp;
+
 
 	@Test
 	void contextLoads() {
+		System.out.println("Context loads with test profile");
+
+		System.out.println("Now printing the sampleProp from test profile: "+sampleProp);
 	}
 
 }
