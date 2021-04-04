cba195acefc5986e2533b0ebab438c3edddd6a0e
Naveen Kumar Anem <naveenanem22@gmail.com>
2021-04-04 16:22:09 +0530

    Externalized config - option1
    Externalized config - option1

--- src/main/java/com/techietipps/ordermgmt/ServletInitializer.java
+++ src/main/java/com/techietipps/ordermgmt/ServletInitializer.java
@@ -5,17 +5,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import java.util.Properties;
+
 @SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ServletInitializer.class);
+		return application.sources(ServletInitializer.class).properties(getProperties());
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServletInitializer.class, args);
 	}
 
+	static Properties getProperties() {
+		Properties props = new Properties();
+		props.put("spring.config.location","file:///D:/Softwares/apache-tomcat-9.0.44-windows-x64/apache-tomcat-9.0.44/conf/application.properties");
+		return props;
+
+	}
 
 }
